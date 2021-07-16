d9a7bdd3d82ad1dd14341519fdb986d29413312d
肖仲文 <xiaozhongwen@mye.hk>
2021-07-16 18:03:25 +0800

    start

--- 02-observable-pg/projects/starter/RxPlayground/RxSwiftPlayground.playground/Contents.swift
+++ 02-observable-pg/projects/starter/RxPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -34,3 +34,34 @@ import RxSwift
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
+
+example(of: "just, of, from") {
+  // 1
+  let one = 1
+  let two = 2
+  let three = 3
+
+  // 2
+    let observable = Observable<Int>.just(one)
+    let observable2 = Observable.of(one, two, three)
+    let observable3 = Observable.of([one, two, three])
+    let observable4 = Observable.from([one, two, three])
+}
+
+example(of: "subscribe") {
+    let one = 1
+    let two = 2
+    let three = 3
+
+    let observable = Observable.of(one, two, three)
+//    observable.subscribe { event in
+//        print(event)
+//        if let element = event.element {
+//            print(element)
+//        }
+//    }
+
+    observable.subscribe(onNext: { element in
+        print(element)
+    })
+}
